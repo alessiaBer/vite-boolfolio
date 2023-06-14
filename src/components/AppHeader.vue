f2c865da3fd288035bca4cee7a0aa15b08b5ea16
alessiaBer <alexdeyn@gmail.com>
2023-06-14 15:14:59 +0200

    [add] AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: "AppHeader"
+};
+</script>
+
+<template>
+  <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-3 shadow align-items-center">
+            <a class="navbar-brand col-md-3 col-lg-2 mr-0" href="#">> Boolfolio</a>
+            <!-- <button class="navbar-toggler d-md-none collapsed ms-auto" type="button" data-toggle="collapse"
+                data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button> -->
+            <ul class="d-flex list-unstyled gap-3 m-0 pe-2">
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contacts</a></li>
+            </ul>
+        </nav>
+</template>
+
+<style scoped></style>
