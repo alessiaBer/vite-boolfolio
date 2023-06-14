409e4a66ec488a612f3203c592c7ebdb8a388c30
alessiaBer <alexdeyn@gmail.com>
2023-06-14 15:53:48 +0200

    [add] route-link to single project

--- src/views/ProjectsList.vue
+++ src/views/ProjectsList.vue
@@ -26,16 +26,23 @@ export default {
     <h1 class="pb-4">My projects</h1>
     <div class="row">
       <div class="col-12" v-for="project in projects">
-        <div class="card d-flex">
+        <div class="project_container d-md-flex p-2 mb-3">
           <div class="card_left">
             <img
               :src="base_api + '/storage/' + project.project_image"
               :alt="project.title"
+              class="w-100"
             />
           </div>
-          <div class="card_right">
+          <div class="card_right ps-md-2">
             <h2>{{ project.title }}</h2>
             <p>{{ project.description }}</p>
+            <router-link
+              :to="{ name: 'single-project', params: { slug: project.slug } }"
+              class="nav-link"
+            >
+              More info
+            </router-link>
           </div>
         </div>
       </div>
