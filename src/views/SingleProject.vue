b2fa863df125ef29cf54a473f7b3d31f2aba72de
alessiaBer <alexdeyn@gmail.com>
2023-06-14 16:43:52 +0200

    [add] SingleProject axios call

--- src/views/SingleProject.vue
+++ src/views/SingleProject.vue
@@ -1,9 +1,28 @@
 <script>
-export default {};
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      project: null,
+      base_api: "http://127.0.0.1:8000/api/projects/",
+    }
+  },
+  mounted() {
+    const url = this.base_api + this.$route.params.slug
+    axios
+    .get(url)
+    .then(response => {
+      this.project = response.data.result
+    })
+    .catch(error => {
+      console.error(error)
+    })
+  }
+};
 </script>
 <template>
-  <div>
-    <h1>Single Project</h1>
+  <div class="container">
+    <h1>{{ project.title }}</h1>
   </div>
 </template>
 
