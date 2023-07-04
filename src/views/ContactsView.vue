05f4b8412c7399d4cd35236ba9bdda7b5515ea5c
alessiaBer <alexdeyn@gmail.com>
2023-07-04 18:14:10 +0200

    [fix] email

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -7,7 +7,8 @@ export default {
       name: '',
       address: '',
       mailContent: '',
-      errors: []
+      errors: [],
+      mailSended: false
     }
   },
   methods: {
@@ -22,6 +23,10 @@ export default {
       .then(response => {
         if (response.data.success) {
           console.log('Bravissima')
+          this.name = ''
+          this.address = ''
+          this.mailContent = ''
+          this.mailSended = true
         } else if (response.data.success === false) {
           console.log(response.data.errors)
           this.errors = response.data.errors
@@ -39,23 +44,24 @@ export default {
 
   <div class="container contacts">
     <h1 class="text-uppercase text-white pb-3">Contacts</h1>
+    <div class="confirm" v-if="mailSended">Messaggio inviato</div>
     <p class="text-white">Do you want to contact me? Please fill the form below!</p>
     <form @submit.prevent="sendMail()">
       <div class="col">
         <div class="mb-3">
           <label for="name" class="form-label text-white">Name</label>
           <input type="text" name="name" id="name" class="form-control" placeholder="Type your name.." aria-describedby="helpId" v-model="name">
-          <small v-if="errors.name">{{ errors.name[0] }}</small>
+          <small v-if="errors.name" class="text-danger">{{ errors.name[0] }}</small>
         </div>
         <div class="mb-3">
           <label for="address" class="form-label text-white">Email</label>
           <input type="mail" name="address" id="address" class="form-control" placeholder="Type your mail.." aria-describedby="helpId" v-model="address">
-          <small v-if="errors.address">{{ errors.address[0] }}</small>
+          <small v-if="errors.address" class="text-danger">{{ errors.address[0] }}</small>
         </div>
         <div class="mb-3">
           <label for="mailContent" class="form-label d-block text-white">Message</label>
           <textarea name="mailContent" id="mailContent" rows="10" class="form-control" placeholder="Type your message.." v-model="mailContent"></textarea>
-          <small v-if="errors.mailContent">{{ errors.mailContent[0] }}</small>
+          <small v-if="errors.mailContent" class="text-danger">{{ errors.mailContent[0] }}</small>
         </div>
       </div>
       <button class="btn btn-dark">Send Message</button>
